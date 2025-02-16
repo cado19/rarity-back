2ae8b51ceb36f3bce1b6a08a68c991b4a6db58f8
cado19 <mryoungrichie@gmail.com>
2025-02-16 12:42:51 +0300

    Properly names fleet include in read single vehicle

--- api/fleet/read_single.php
+++ api/fleet/read_single.php
@@ -7,14 +7,14 @@ header('Content-Type: application/json');
 
 // include necessary files
 include_once '../../config/Database.php';
-include_once '../../models/fleet.php';
+include_once '../../models/Fleet.php';
 
 // Instantiate The DB and connect to it
 $database = new Database();
 $db       = $database->connect();
 
 // instantiate blog fleet object
-$fleet = new fleet($db);
+$fleet = new Fleet($db);
 
 // get the id from the url
 
@@ -29,18 +29,18 @@ $fleet->read_single();
 
 // create the array
 $fleet_arr = [
-    'id'              => $fleet->id,
-    'make'            => $fleet->make,
-    'model'           => $fleet->model,
-    'number_plate'    => $fleet->number_plate,
-    'seats'           => $fleet->seats,
-    'drive_train'     => $fleet->drive_train,
-    'category_id'     => $fleet->category_id,
-    'category_name'   =>$fleet->category_name,
-    'daily_rate'      => $fleet->daily_rate,
-    'vehicle_excess'  => $fleet->vehicle_excess,
-    'transmission'    => $fleet->transmission,
-    'fuel'            => $fleet->fuel,
+    'id'             => $fleet->id,
+    'make'           => $fleet->make,
+    'model'          => $fleet->model,
+    'number_plate'   => $fleet->number_plate,
+    'seats'          => $fleet->seats,
+    'drive_train'    => $fleet->drive_train,
+    'category_id'    => $fleet->category_id,
+    'category_name'  => $fleet->category_name,
+    'daily_rate'     => $fleet->daily_rate,
+    'vehicle_excess' => $fleet->vehicle_excess,
+    'transmission'   => $fleet->transmission,
+    'fuel'           => $fleet->fuel,
 
 ];
 
