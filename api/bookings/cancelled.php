50be09b4314bbd87a95af49c3e7405e60bf7fcec
cado19 <mryoungrichie@gmail.com>
2025-03-25 17:36:34 +0300

    added data to cancelled bookings api endpoint

---
+++ api/bookings/cancelled.php
@@ -0,0 +1,61 @@
+<?php
+// THIS FILE WILL DELIVER CANCELLED BOOKINGS TO EXTERNAL REQUESTS
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// include necessary files
+include_once '../../config/Database.php';
+include_once '../../models/Booking.php';
+
+// Instantiate The DB and connect to it
+$database = new Database();
+$db       = $database->connect();
+
+// print_r($db);
+
+// instantiate blog post object
+$booking = new Booking($db);
+
+// vehicles query as a function
+$result = $booking->read_cancelled();
+
+// get row count
+$num = $result->rowCount();
+
+//check if any posts
+
+if ($num > 0) {
+    $booking_arr             = [];
+    $booking_arr['bookings'] = []; //this is where the data will go
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        // single post item array
+        $booking_item = [
+            'id'           => $id,
+            'booking_no'   => $booking_no,
+            'make'         => $make,
+            'model'        => $model,
+            'number_plate' => $number_plate,
+            'c_fname'      => $c_fname,
+            'c_lname'      => $c_lname,
+            'start_date'   => $start_date,
+            'end_date'     => $end_date,
+        ];
+
+        // push that post item to 'data' index of array
+        array_push($booking_arr['bookings'], $booking_item);
+
+    }
+    // convert the posts to json
+    echo json_encode($booking_arr);
+} else {
+    // No posts found in the database ($num = 0)
+    $response = [
+        'messsage' => 'No posts found',
+    ];
+    echo json_encode($response);
+}
