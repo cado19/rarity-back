2c6b0aa7849051c1dc8f3217c43f6fa3876a2584
Richie Ong'anda <mryoungrichie@gmail.com>
2026-01-26 09:34:53 +0300

    updated file path

--- api/contracts/update.php
+++ api/contracts/update.php
@@ -30,9 +30,8 @@ $response = [];
 $data = json_decode(file_get_contents('php://input'));
 // error_log(print_r($data, true)); // log the object to PHP error log
 
-
-$contract->booking_id = $data->id; // set booking id property of contract class 
-$contract->contract_to_sign(); // get id of contract from booking id set above
+$contract->booking_id = $data->id; // set booking id property of contract class
+$contract->contract_to_sign();     // get id of contract from booking id set above
 
 // echo json_encode($data);
 
@@ -52,37 +51,36 @@ if (empty($data->image)) {
     $name_file = 'signature_' . date("his") . '.png';
 
     // $filePath = '../../files/signatures/' . $name_file;
-    $filePath = __DIR__ . '/../../../rarity-client/contract/signatures/' . $name_file;
-
+    $uploadDir = realpath(__DIR__ . '/../../../rarity-client/contract/signatures//');
+    $filePath  = $uploadDir . '/' . $name_file;
+    // $filePath = __DIR__ . '/../../../rarity-client/contract/signatures/' . $name_file;
 
     if (file_put_contents($filePath, $imageData)) {
-    	$contract->signature = $name_file;
-    	if($contract->sign_contract()){
-		    // signature uploaded and file name saved to db
-		    $message             = "Contract successfully signed";
-		    $status              = 'Success';
-		    $response['message'] = $message;
-		    $response['status']  = $status;
-		    echo json_encode($response);
-    	} else {
-    		// signature uploaded but file name not saved to db
-		    $message             = "An error occured. Please try again";
-		    $status              = 'Error';
-		    $response['message'] = $message;
-		    $response['status']  = $status;
-		    echo json_encode($response);
-    	}
+        $contract->signature = $name_file;
+        if ($contract->sign_contract()) {
+            // signature uploaded and file name saved to db
+            $message             = "Contract successfully signed";
+            $status              = 'Success';
+            $response['message'] = $message;
+            $response['status']  = $status;
+            echo json_encode($response);
+        } else {
+            // signature uploaded but file name not saved to db
+            $message             = "An error occured. Please try again";
+            $status              = 'Error';
+            $response['message'] = $message;
+            $response['status']  = $status;
+            echo json_encode($response);
+        }
 
         // echo json_encode(['success' => true, 'file' => $filePath]);
     } else {
-    	// signature not uploaded 
-		    $message             = "An error occured. Signature not uploaded ";
-		    $status              = 'Error';
-		    $response['message'] = $message;
-		    $response['status']  = $status;
-		    echo json_encode($response);
+        // signature not uploaded
+        $message             = "An error occured. Signature not uploaded ";
+        $status              = 'Error';
+        $response['message'] = $message;
+        $response['status']  = $status;
+        echo json_encode($response);
         // echo json_encode(['success' => false, 'message' => 'Failed tosavetheimage . ']);
     }
 }
-
-
