863a730b4d530722685e098076c6a8fccaf0c01b
Richie Ong'anda <mryoungrichie@gmail.com>
2026-05-17 09:55:48 +0300

    First tentative fix for voucher downloads

--- api/bookings/download.php
+++ api/bookings/download.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require '../../vendor/autoload.php'; // Dompdf
 include_once '../../config/cors.php';
 include_once '../../config/Database.php';
@@ -31,15 +31,21 @@ if ($voucher['fuel'] > 0) {
     $ultimate_total += $voucher['fuel'];
 }
 
-if ($voucher['subtotal'] > 0) {
-    $ultimate_total += $voucher['subtotal'];
-} else {
+if ($voucher['vat'] > 0) {
     $ultimate_total += $voucher['total'];
+} else {
+    $ultimate_total += $voucher['subtotal'];
 }
 
 // Build HTML
 $html = "
-    <div style='text-align:center; margin-bottom:20px;'>
+    <style>
+      p {
+        margin: 2px 0;
+        padding: 0;
+      }
+    </style>
+    <div style='text-align:center; margin-bottom:10px;'>
         <img src='https://backend.raritycars.com/files/rarity_contract_top.png'
              alt='Company Logo' style='width:100%;' />
         <h2 style='margin:0;'>Booking Voucher</h2>
@@ -69,7 +75,7 @@ if ($voucher['cdw_total'] > 0) {
 }
 
 if ($voucher['vat'] > 0) {
-    $html .= "<p><b>Booking Fee:</b> Ksh. {$voucher['total']}/-</p>
+    $html .= "<p><b>Booking Fee:</b> Ksh. {$voucher['subtotal']}/-</p>
               <p><b>VAT:</b> Ksh. {$voucher['vat']}/-</p>";
 }
 
@@ -79,17 +85,50 @@ $html .= "
     <p><b>End Date:</b> {$voucher['end_date']}</p>
     <p><b>Start Time:</b> {$voucher['start_time']}</p>
     <p><b>End Time:</b> {$voucher['end_time']}</p>
-    <h3>PAYMENT DETAILS:</h3>
-    <p><b>BANK NAME:</b> I&M BANK</p>
-    <p><b>BRANCH:</b> VALLEY ARCADE</p>
-    <p><b>Account Name:</b> RARITY TRAVEL LTD</p>
-    <p><b>ACCOUNT NUMBER:</b> 01605023636350 (KES)</p>
-    <p><b>ACCOUNT NUMBER:</b> 01605023631250 (USD)</p>
-    <p><b>S.W.I.F.T BIC:</b> IMBLKENA</p>
-    <p><b>BRANCH CODE:</b> 016</p>
-    <p><b>BANK CODE:</b> 057</p>
-    <p><b>MPESA PAYBILL:</b> 400200</p>
-    <p><b>ACCOUNT:</b> 40044610</p>
+    <style>
+      #payment-details {
+        border: 3px solid #000;
+        border-radius: 8px;
+        padding: 10px;
+        margin-top: 15px;
+      }
+      #payment-details p {
+        margin: 0;
+        padding: 2px 0;
+      }
+      #payment-details h3 {
+        margin-bottom: 8px;
+        text-align: center;
+        text-decoration: underline;
+      }
+      #payment-details a {
+        display: inline-block;
+        margin-top: 10px;
+        padding: 6px 12px;
+        border: 2px solid #198754;
+        border-radius: 6px;
+        color: #198754;
+        text-decoration: none;
+        font-weight: bold;
+      }
+      #payment-details a:hover {
+        background-color: #198754;
+        color: #fff;
+      }
+    </style>
+    <div id="payment-details">
+        <h3>PAYMENT DETAILS:</h3>
+        <p><b>BANK NAME:</b> I&M BANK</p>
+        <p><b>BRANCH:</b> VALLEY ARCADE</p>
+        <p><b>Account Name:</b> RARITY TRAVEL LTD</p>
+        <p><b>ACCOUNT NUMBER:</b> 01605023636350 (KES)</p>
+        <p><b>ACCOUNT NUMBER:</b> 01605023631250 (USD)</p>
+        <p><b>S.W.I.F.T BIC:</b> IMBLKENA</p>
+        <p><b>BRANCH CODE:</b> 016</p>
+        <p><b>BANK CODE:</b> 057</p>
+        <p><b>MPESA PAYBILL:</b> 400200</p>
+        <p><b>ACCOUNT:</b> 40044610</p>
+    </div>
 ";
 
 // Configure Dompdf
